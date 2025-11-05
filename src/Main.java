149d2d097b92410b1ec530342d920bbc821ace62
origin <dubnitskaya.katya@mail.ru>
2025-11-05 09:49:02 +0400

    New1

--- src/Main.java
+++ src/Main.java
@@ -19,45 +19,57 @@ public class Main {
 
         System.out.println("Задача 1.2");
         double[] second = {1.57, 7.654, 9.986};
-        for (int i = 0; i < second.length; i++) {
-            System.out.println(second[i]);
+        for (int l = 0; l < second.length; l++) {
+            System.out.println(second[l]);
         }
+
+        System.out.println("Задача 1.3");
+
+        int[] third = {0, 80, 90, 120, 200};
+        for (int r = 0; r < third.length; r++) {
+            System.out.println(third[r]);
+        }
+
 
         System.out.println("Задача 2");
         System.out.println(Arrays.toString(weight));
         System.out.println(Arrays.toString(second));
+        System.out.println(Arrays.toString(third));
 
-        System.out.println("Задача 3");
-        for (int j = 2; j>=0; j--) {
-            if (weight[j] == j + 1) {
-                System.out.print(weight[j] + " ");
 
-            }
+        System.out.println("\n");
+        System.out.println("Задача 3.1");
+        int j;
+        for (j = weight.length - 1; j >= 0; j--) {
+            System.out.print(weight[j] + ",");
         }
         System.out.println("\n");
 
-        System.out.println("Задача 3.1");
-        for (int f = 2; f >= 0; f--) {
+        System.out.println("Задача 3.2");
+        for (int f = second.length - 1; f >= 0; f--) {
+            System.out.print(second[f] + ",");
 
-                System.out.print(second[f] + " ");
 
+        }
+        System.out.println("\n");
+
+        System.out.println("Задача 3.3");
+        for (int m = third.length - 1; m >= 0; m--) {
+            System.out.print(third[m] + ",");
         }
         System.out.println("\n");
         System.out.println("Задача 4");
-        for (int d=0; d <weight.length; d++){
-            if (weight[d]%2!=0) {
+        for (int d = 0; d < weight.length; d++) {
+            if (weight[d] % 2 != 0) {
                 weight[d] = weight[d] + 1;
             }
-                System.out.println(Arrays.toString(weight));
-
-
-
+            System.out.println(Arrays.toString(weight));
         }
 
 
+    }
+}
 
-            }
-        }
 
 
 
